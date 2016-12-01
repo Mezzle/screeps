39b0791c875b5ff8d8f556e7ec4184e8a91db601
Martin Meredith <martin@sourceguru.net>
2016-12-01 22:33:01 +0000

    Respawn upgraders

--- main.js
+++ main.js
@@ -5,27 +5,39 @@ module.exports.loop = function () {
 
     let name;
     for (name in Memory.creeps) {
-        if (!Game.creeps[name]) {
-            delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memory:', name);
+        if (Memory.creeps.hasOwnProperty(name)) {
+            if (!Game.creeps[name]) {
+                delete Memory.creeps[name];
+                console.log('Clearing non-existing creep memory:', name);
+            }
         }
     }
 
     const harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
     console.log('Harvesters: ' + harvesters.length);
 
+    const upgraders = _.filter(Game.creeps, (creep) => creep.memory.role == 'upgrader');
+    console.log('Harvesters: ' + harvesters.length);
+
     if (harvesters.length < 2) {
-        const newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'harvester'});
+        const newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
         console.log('Spawning new harvester: ' + newName);
     }
 
+    if (upgraders.length < 1) {
+        const newName = Game.spawns['Spawn1'].createCreep([WORK, CARRY, MOVE], undefined, {role: 'upgrader'});
+        console.log('Spawning new upgrader: ' + newName);
+    }
+
     for (name in Game.creeps) {
-        const creep = Game.creeps[name];
-        if (creep.memory.role == 'harvester') {
-            roleHarvester.run(creep);
-        }
-        if (creep.memory.role == 'upgrader') {
-            roleUpgrader.run(creep);
+        if (Game.creeps.hasOwnProperty(name)) {
+            const creep = Game.creeps[name];
+            if (creep.memory.role == 'harvester') {
+                roleHarvester.run(creep);
+            }
+            if (creep.memory.role == 'upgrader') {
+                roleUpgrader.run(creep);
+            }
         }
     }
 };
