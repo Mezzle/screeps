b55840fda45303eb36aad56505ae75c5a292188c
Martin Meredith <martin@justsayplease.co.uk>
2016-12-02 15:26:16 +0000

    add Beije

--- action.harvest.js
+++
@@ -1,8 +0,0 @@
-module.exports =  function (creep) {
-
-    const target = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE);
-
-    if (creep.harvest(target) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(target);
-    }
-};
--- action.upgrade.js
+++
@@ -1,5 +0,0 @@
-module.exports = (creep) => {
-    if (creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-        creep.moveTo(creep.room.controller);
-    }
-};
