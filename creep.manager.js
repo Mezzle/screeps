f145e9b44ad678c79be799952499853726e5125c
Martin Meredith <martin@sourceguru.net>
2016-12-02 00:58:32 +0000

    let k

--- creep.manager.js
+++ creep.manager.js
@@ -35,6 +35,7 @@ module.exports = new class {
             }
         }
 
+        let k;
         for (k in this.roles) {
             if (this.roles.hasOwnProperty(k)) {
                 let roleCount = _.filter(Game.creeps, (creep) => creep.memory.role == k);
