59c9544843add851c2cc140723147decf3bd5b72
Martin Meredith <martin@sourceguru.net>
2016-12-02 01:18:54 +0000

    Use actions array

--- role.builder.js
+++ role.builder.js
@@ -29,7 +29,7 @@ module.exports = new class extends BaseCreep {
             }
         }
         else {
-            harvest(creep);
+            this.actions.harvest(creep);
         }
     }
 };
