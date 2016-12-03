6ca439912fbe5e3a64eedcf4c953620d6d2ef13e
Martin Meredith <martin@sourceguru.net>
2016-12-03 00:18:15 +0000

    Update Upgrader.

--- role.upgrader.js
+++ role.upgrader.js
@@ -6,7 +6,7 @@ module.exports = new class extends BaseCreep {
         super();
         this.role = 'upgrader';
 
-        this.limit = 0;
+        this.limit = 1;
     }
 
     /** @param {Creep} creep **/
@@ -25,7 +25,21 @@ module.exports = new class extends BaseCreep {
             this.actions.upgrade(creep);
         }
         else {
-            this.actions.harvest(creep);
+            target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.structureType == STRUCTURE_STORAGE ||
+                        structure.structureType == STRUCTURE_SPAWN ||
+                        structure.structureType == STRUCTURE_TOWER) && structure.energy > 0;
+                }
+            });
+
+            if (target) {
+                if (creep.withdraw(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0]);
+                }
+            } else {
+                this.actions.harvest(creep);
+            }
         }
     }
 };
